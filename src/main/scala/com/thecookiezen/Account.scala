1dadf7238b29ca3e95b5df0ec34effdba8b79e19
kornelrabczak <1546480+kornelrabczak@users.noreply.github.com>
2020-09-20 16:57:19 +0200

    Fixes order of parameters for the check if there is enough money on the account

--- src/main/scala/com/thecookiezen/Account.scala
+++ src/main/scala/com/thecookiezen/Account.scala
@@ -35,10 +35,10 @@ object Account {
   val checkBalance: CheckBalance                 = calculateBalance => account => calculateBalance(account.transactionLog)
   val calculateBalance: CalculateBalance         = _.foldLeft(BigDecimal(0))(_ + _.amount)
   val checkIfEnoughBalance: CheckIfEnoughBalance = balance =>
-    transfer => Either.cond(balance - transfer > 0, (), InsufficientBalance)
+    transfer => Either.cond(balance + transfer > 0, (), InsufficientBalance)
 
   val checkIfAccountHasEnoughBalance: CheckIfAccountHasEnoughBalance = calculate =>
-    balance => isEnough => account => withdraw => isEnough(withdraw.amount)(balance(calculate)(account)).map(_ => account)
+    balance => isEnough => account => withdraw => isEnough(balance(calculate)(account))(withdraw.amount).map(_ => account)
 
   val accountService: Account => Withdraw => Either[AccountError, Account] = checkIfAccountHasEnoughBalance(calculateBalance)(checkBalance)(checkIfEnoughBalance)
 }
