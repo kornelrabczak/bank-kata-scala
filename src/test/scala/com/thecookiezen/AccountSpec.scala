31807a89d4dac84065da66e8ff5c2a656bede239
kornelrabczak <1546480+kornelrabczak@users.noreply.github.com>
2020-09-20 16:57:45 +0200

    Adds tests for the Account logic

---
+++ src/test/scala/com/thecookiezen/AccountSpec.scala
@@ -0,0 +1,35 @@
+package com.thecookiezen
+
+import java.time.LocalDate
+
+import com.thecookiezen.Bank.Money
+import com.thecookiezen.Account.AccountId
+import com.thecookiezen.InsufficientBalance
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+class AccountSpec extends AnyFlatSpec with Matchers {
+
+  private val clock: Clock                                        = () => LocalDate.parse("2020-08-01")
+  private val depositTransaction: (AccountId, Money) => Deposit   = Transaction.deposit(clock)
+  private val withdrawTransaction: (AccountId, Money) => Withdraw = Transaction.withdraw(clock)
+
+  "Account" should "calculate right balance from a transactions" in {
+    Account.calculateBalance(
+      List(
+        depositTransaction("someId", 123),
+        withdrawTransaction("someId", 100),
+        depositTransaction("someId", 7)
+      )
+    ) shouldBe 30
+  }
+
+  it should "check if there is enough balance for the withdraw" in {
+    Account.checkIfEnoughBalance(100)(-23) shouldBe Right(())
+  }
+
+  it should "return error if there is not enough balance for the withdraw" in {
+    Account.checkIfEnoughBalance(23)(-100) shouldBe Left(InsufficientBalance)
+  }
+}
